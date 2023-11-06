ee2625c5185db073acd39fc4efee730ac5ed469d
Sudip Das <sudip5428@gmail.com>
2023-11-06 19:35:11 +0530

    code commit for output file cheanges

---
+++ aws-output/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+}
+
+output "public_ip" {
+  value = "aws_instance.example.public_ip"
+}
